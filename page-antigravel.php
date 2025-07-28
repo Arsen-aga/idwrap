557dabd42b9a7038fe180c216d1f030bf3ab65bf
Arsen Asanov <tatarin08082002@gmail.com>
2025-07-28 15:53:54 +0300

    Добавил карточки в блок Проверенных материалов verified-materials

--- page-antigravel.php
+++ page-antigravel.php
@@ -3417,96 +3417,238 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <section class="verified-materials">
-    <div class="container">
-      <h2 class="verified-materials__title _title">
-        Используем только проверенные<br>
-        материалы от ведущих мировых<br>
-        производителей
-      </h2>
-    </div>
-    <div class="cursor">
-      <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
-    </div>
-    <div class="verified-materials__elems-wrapper">
+  <div class="space-sec"></div>
+  <div class="wrapper-brown">
+    <section class="verified-materials">
       <div class="container">
-        <div class="verified-materials__elems">
-          <div class="verified-materials__elem">
-            <div class="verified-materials__elem-icon">
-              <?php include(get_template_directory() . '/assets/images/icons/verified-materials-1.svg'); ?>
-            </div>
-            <div class="verified-materials__elem-info">
-              <h4 class="verified-materials__elem-title">
-                Толщина пленок<br>
-                от 200 микрон,
-              </h4>
-              <p class="verified-materials__elem-text">
-                что обеспечивает надежную<br>
-                защиту и долговечность
-              </p>
-            </div>
-          </div>
-          <div class="verified-materials__elem">
-            <div class="verified-materials__elem-icon">
-              <?php include(get_template_directory() . '/assets/images/icons/verified-materials-2.svg'); ?>
-            </div>
-            <div class="verified-materials__elem-info">
-              <h4 class="verified-materials__elem-title">
-                С каждым производителем<br>
-                сотрудничаем более 5 лет,
-              </h4>
-              <p class="verified-materials__elem-text">
-                а долговечность пленок<br>
-                протестирована на наших<br>
-                собственных автомобилях
-              </p>
-            </div>
-          </div>
-          <div class="verified-materials__elem">
-            <div class="verified-materials__elem-icon">
-              <?php include(get_template_directory() . '/assets/images/icons/verified-materials-3.svg'); ?>
-            </div>
-            <div class="verified-materials__elem-info">
-              <h4 class="verified-materials__elem-title">
-                Большое разнообразие<br>
-                пленок в наличии,
-              </h4>
-              <p class="verified-materials__elem-text">
-                что позволяет приступить<br>
-                к оклейке уже сегодня
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="cursor">
-        <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
-      </div>
-      <div class="verified-materials__partners">
-        <div class="verified-materials__partner">
-          <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-1.webp" alt="">
-        </div>
-        <div class="verified-materials__partner">
-          <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-2.webp" alt="">
-        </div>
-        <div class="verified-materials__partner">
-          <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-3.webp" alt="">
-        </div>
-        <div class="verified-materials__partner">
-          <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-4.webp" alt="">
-        </div>
-        <div class="verified-materials__partner">
-          <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-5.webp" alt="">
-        </div>
+        <h2 class="verified-materials__title _title">
+          Используем только проверенные<br>
+          материалы от ведущих мировых<br>
+          производителей
+        </h2>
       </div>
       <div class="cursor">
         <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
       </div>
-      
-    </div>
-  </section>
+      <div class="verified-materials__elems-wrapper">
+        <div class="container">
+          <div class="verified-materials__elems">
+            <div class="verified-materials__elem">
+              <div class="verified-materials__elem-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/verified-materials-1.svg'); ?>
+              </div>
+              <div class="verified-materials__elem-info">
+                <h4 class="verified-materials__elem-title">
+                  Толщина пленок<br>
+                  от 200 микрон,
+                </h4>
+                <p class="verified-materials__elem-text">
+                  что обеспечивает надежную<br>
+                  защиту и долговечность
+                </p>
+              </div>
+            </div>
+            <div class="verified-materials__elem">
+              <div class="verified-materials__elem-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/verified-materials-2.svg'); ?>
+              </div>
+              <div class="verified-materials__elem-info">
+                <h4 class="verified-materials__elem-title">
+                  С каждым производителем<br>
+                  сотрудничаем более 5 лет,
+                </h4>
+                <p class="verified-materials__elem-text">
+                  а долговечность пленок<br>
+                  протестирована на наших<br>
+                  собственных автомобилях
+                </p>
+              </div>
+            </div>
+            <div class="verified-materials__elem">
+              <div class="verified-materials__elem-icon">
+                <?php include(get_template_directory() . '/assets/images/icons/verified-materials-3.svg'); ?>
+              </div>
+              <div class="verified-materials__elem-info">
+                <h4 class="verified-materials__elem-title">
+                  Большое разнообразие<br>
+                  пленок в наличии,
+                </h4>
+                <p class="verified-materials__elem-text">
+                  что позволяет приступить<br>
+                  к оклейке уже сегодня
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="cursor">
+          <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
+        </div>
+        <div class="verified-materials__partners">
+          <div class="verified-materials__partner">
+            <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-1.webp" alt="">
+          </div>
+          <div class="verified-materials__partner">
+            <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-2.webp" alt="">
+          </div>
+          <div class="verified-materials__partner">
+            <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-3.webp" alt="">
+          </div>
+          <div class="verified-materials__partner">
+            <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-4.webp" alt="">
+          </div>
+          <div class="verified-materials__partner">
+            <img class="verified-materials__partner-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-5.webp" alt="">
+          </div>
+        </div>
+        <div class="cursor">
+          <?php include(get_template_directory() . '/assets/images/icons/cursor.svg'); ?>
+        </div>
+        <div class="verified-materials__items">
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-1.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Глянцевые пленки
+              </h4>
+              <p class="verified-materials__item-text">
+                — придают кузову яркий блеск,<br> усиливая глубину цвета и создавая<br> эффект зеркальной поверхности
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                оклейки глянцевой пленкой
+              </a>
+            </div>
+          </div>
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-2.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Матовые пленки
+              </h4>
+              <p class="verified-materials__item-text">
+                — придают автомобилю стильный<br>
+                и современный вид, подчеркивая<br>
+                его формы
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                оклейки матовой пленкой
+              </a>
+            </div>
+          </div>
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-3.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Цветные пленки
+              </h4>
+              <p class="verified-materials__item-text">
+                — позволяют изменить цвет<br>
+                кузова, при этом сохраняя<br>
+                защитные свойства
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                оклейки цветной пленкой
+              </a>
+            </div>
+          </div>
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-4.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Винилография
+              </h4>
+              <p class="verified-materials__item-text">
+                — нанесение декоративных<br> виниловых пленок для создания<br> уникального дизайна Винилография
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                винилографии
+              </a>
+            </div>
+          </div>
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-5.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Перламутровые пленки
+              </h4>
+              <p class="verified-materials__item-text">
+                — создают эффект переливающихся<br> цветов, добавляя автомобилю<br> эксклюзивности
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                оклейки перламутровой пленкой
+              </a>
+            </div>
+          </div>
+          <div class="verified-materials__item">
+            <div class="verified-materials__item-wrapper">
+              <img class="verified-materials__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/verified-materials-item-6.webp" alt="">
+            </div>
+            <div class="verified-materials__item-info">
+              <h4 class="verified-materials__item-title">
+                Карбоновые пленки
+              </h4>
+              <p class="verified-materials__item-text">
+                — виниловое покрытие,<br> имитирующее текстуру и внешний<br> вид карбона
+              </p>
+              <a class="verified-materials__item-btn _main-btn" href="#">
+                Рассчитать стоимость<br>
+                оклейки карбоновой пленкой
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="verified-materials__blocks">
+          <div class="verified-materials__block">
+            <h4 class="verified-materials__block-title">
+              Хотите <span>частично восстановить<br></span>
+              уже нанесенную пленку?
+            </h4>
+            <p class="verified-materials__block-text">'
+              Подберем пленку, учитывая все особенности<br>
+              оригинала — цвет, текстуру и производителя
+            </p>
+            <a class="verified-materials__block-btn _main-btn" href="#">
+              Рассчитать стоимость<br>
+              восстановления пленки
+            </a>
+          </div>
+          <div class="verified-materials__block">
+            <h4 class="verified-materials__block-title">
+              Хотите <span>улучшить состояние<br></span>
+              и внешний вид пленки?
+            </h4>
+            <p class="verified-materials__block-text">'
+              Проведет детейлинг-мойку, полировку пленки<br>
+              и нанесение покрытия для блеска
+            </p>
+            <a class="verified-materials__block-btn _main-btn" href="#">
+              Рассчитать стоимость<br>
+              улучшения состояния пленки
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="divider"></div>
+    
+  </div>
 
 
 
