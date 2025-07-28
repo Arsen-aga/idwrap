76245a969c29a1b64025d680b8e33dd5437a04d3
Arsen Asanov <tatarin08082002@gmail.com>
2025-07-28 21:59:54 +0300

    Добавил блок качество оклейки quality-pasting

--- page-antigravel.php
+++ page-antigravel.php
@@ -11,7 +11,7 @@
 get_header(); ?>
 <!-- Сингловая пленок -->
 <main class="main" id="skin-single">
-  <section class="sec-0">
+  <section class="sec-0 relative">
     <div class="sec-0__container container">
       <div class="sec-0__layout">
         <div class="sec-0__content">
@@ -3417,7 +3417,6 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <div class="space-sec"></div>
   <div class="wrapper-brown">
     <section class="verified-materials">
       <div class="container">
@@ -4235,7 +4234,82 @@ get_header(); ?>
       </div>
     </div>
   </section>
-
+  <div class="wrapper-brown">
+    <section class="quality-pasting">
+      <div class="container">
+        <h2 class="quality-pasting__title _title">
+          Хотите увидеть процесс лично<br>
+          и убедиться в качестве оклейки?
+        </h2>
+        <div class="quality-pasting__items">
+          <div class="quality-pasting__item">
+            <div class="quality-pasting__item-wrapper">
+              <img class="quality-pasting__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec-09/excurs.webp" alt="">
+            </div>
+            <div class="quality-pasting__item-info">
+              <h4 class="quality-pasting__item-title">
+                Проведем экскурсию<br>
+                по детейлинг-студии
+              </h4>
+              <p class="quality-pasting__item-text">
+                Приглашаем вас посетить нашу<br> студию, где вы сможете увидеть <br>автомобили, находящиеся в работе,<br>
+                и ознакомиться с процессом<br> разборки и сборки
+              </p>
+              <address class="footer__col-address quality-pasting__item-address">
+                <?php include(get_template_directory() . '/assets/images/icons/geo.svg'); ?>
+                <p>ул. Архитектора Щусева, 5, к1</p>
+              </address>
+              <a class="quality-pasting__item-btn _main-btn" href="">
+                Записаться на экскурсию<br>
+                по детелинг-студии
+              </a>
+            </div>
+          </div>
+          <div class="quality-pasting__item">
+            <div class="quality-pasting__item-info">
+              <h4 class="quality-pasting__item-title">
+                Предоставим видеоотчеты<br>
+                выполненных оклеек,
+              </h4>
+              <p class="quality-pasting__item-text">
+                в которых вы увидите все этапы —<br>
+                от процесса разборки до оклейки<br>
+                и сборки автомобиля
+              </p>
+              <a class="quality-pasting__item-btn _main-btn" href="">
+                Получить видеоотчеты<br>
+                выполненных оклеек
+              </a>
+            </div>
+            <div class="quality-pasting__item-wrapper">
+              <img class="quality-pasting__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec-09/report.webp" alt="">
+            </div>
+          </div>
+          <div class="quality-pasting__item">
+            <div class="quality-pasting__item-wrapper">
+              <img class="quality-pasting__item-img _img" src="<?php echo get_template_directory_uri(); ?>/assets/images/sec-09/translation.webp" alt="">
+            </div>
+            <div class="quality-pasting__item-info">
+              <h4 class="quality-pasting__item-title">
+                Организуем онлайн- <br>
+                трансляцию из студии<br>
+                из детейлинга
+              </h4>
+              <p class="quality-pasting__item-text">
+                Специалист выйдет с вами<br>
+                на видеосвязь прямо из студии <br>детейлинга и покажет любой <br>интересующий процесс
+              </p>
+              <a class="quality-pasting__item-btn _main-btn" href="">
+                Согласовать время<br>
+                видеосозвона
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="space-sec"></div>
+  </div>
 
   <div class="space-sec"></div>
   <section class="section sec-12">
