93256655eddda0d546573473cfa3c00dc7c2849a
PersAnd12 <andrejprasolov93239@gmail.com>
2025-07-29 13:21:01 +0300

    Вынес блоки в темплейты и подключил на типовой оклейки(page-antigravel)

---
+++ template-parts/sections/sec-11.php
@@ -0,0 +1,39 @@
+<section class="section sec-11 _sec-light">
+  <div class="sec-11__background _image-abs">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sec-11/background.svg" alt=" Изображение - логотип на фон">
+  </div>
+  <div class="sec-11__container container relative">
+    <div class="sec-11__content">
+      <div class="sec-11__element">
+        <div class="_image-abs">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sec-11/docs.webp" alt=" Изображение - документы на фон">
+        </div>
+      </div>
+      <h2 class="sec-11__title _title">
+        Гарантия <br>
+        действует <br>
+        <span>на весь срок <br>
+          эксплуатации <br>
+          автомобиля:</span>
+      </h2>
+      <ul class="sec-11__list">
+        <li>
+          <p class="sec-11__info _card-info">
+            гарантия на <span>качество оклейки: </span><br>
+            отсутствие вздутий пленки или отклеиваний
+          </p>
+        </li>
+        <li>
+          <p class="sec-11__info _card-info">
+            гарантия на <span>качество сборки </span>автомобиля
+          </p>
+        </li>
+        <li>
+          <p class="sec-11__info _card-info">
+            гарантия на <span>оригинальность пленки</span>
+          </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
