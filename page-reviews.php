7f1b2e4a9f67ad9603e7329ddb9ccde8a1b8df53
PersAnd12 <andrejprasolov93239@gmail.com>
2025-07-30 19:11:22 +0300

    Собрал страницы - Отзывы, Акции

---
+++ page-reviews.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<main>
+
+  <?php get_template_part('template-parts/sections/default-first-block'); ?>
+
+  <?php get_template_part('template-parts/sections/sec-2'); ?>
+
+  <?php get_template_part('template-parts/sections/video-reviews'); ?>
+
+  <div class="space-sec"></div>
+
+  <?php get_template_part('template-parts/sections/sec-12'); ?>
+
+  <?php get_template_part('template-parts/sections/sec-14'); ?>
+
+  <?php get_template_part('template-parts/sections/map'); ?>
+
+</main>
+
+<?php get_footer(); ?>
---
+++ template-parts/sections/default-first-block.php
@@ -0,0 +1,32 @@
+<section class="front-block _image-wrapper" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/front-bg-2.webp);">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="front-block__inner relative">
+      <div class="breadcrumbs">
+        <ul class="breadcrumbs__list">
+          <li class="breadcrumbs__point">
+            <a class="breadcrumbs__link" href="#">Главная</a>
+          </li>
+          <li class="breadcrumbs__point">
+            <a class="breadcrumbs__link active" href="#">Акции</a>
+          </li>
+        </ul>
+      </div>
+      <div class="front-block__top">
+        <h1 class="front-block__title _title-1">
+          Акции студии детейлинга
+        </h1>
+        <?php include(get_template_directory() . '/assets/images/icons/logo.svg'); ?>
+      </div>
+      <div class="front-block__btns">
+        <a class="front-block__btn _main-btn" href="#callback-modal" data-fancybox>
+          Получить промокод на скидку
+        </a>
+        <a class="front-block__link" href="https://wa.me/+79999999" target="_blank">
+          <?php include(get_template_directory() . '/assets/images/icons/whatsapp.svg'); ?>
+          <span>Или получите консультацию</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
