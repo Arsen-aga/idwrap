62f6a3ed6725b2e6d7d4c8ff1d6f4860c35a537d
Arsen Asanov <tatarin08082002@gmail.com>
2025-07-30 13:09:26 +0300

    добавил страницу блога

--- page-blocks.php
+++ page-blocks.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
 
 <main class="main" id="skin-single">
+  <?php get_template_part('sourse/sections/type-first-block'); ?>
 
+
+
+    <?php get_template_part('sourse/sections/skin-happen'); ?>
+
+    
 
 </main>
 
