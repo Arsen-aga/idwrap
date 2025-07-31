4fb401746da4aa150e795ac0afd9585d9001115b
PersAnd12 <andrejprasolov93239@gmail.com>
2025-07-31 17:41:55 +0300

    Написал функцию для вывода блоков в определенном порядке

--- custom-page-antigravel.php
+++ custom-page-antigravel.php
@@ -13,80 +13,40 @@ get_header(); ?>
 <!-- Сингловая пленок -->
 <main class="main" id="skin-single">
 
-  <?php get_template_part('template-parts/sections/type-first-block'); ?>
-
-  <?php get_template_part('template-parts/sections/carcase-info'); ?>
-
-  <?php get_template_part('template-parts/sections/skin-option'); ?>
-
-  <div class="wrapper-brown">
-
-    <?php get_template_part('template-parts/sections/skin-happen'); ?>
-
-    <div class="space-sec"></div>
-
-    <?php get_template_part('template-parts/sections/skin-listimg'); ?>
-
-  </div>
-
-  <?php get_template_part('template-parts/sections/pasting-of-any-complexity'); ?>
-
-  <?php get_template_part('template-parts/sections/full-wrapping'); ?>
-
-  <div class="wrapper-brown">
-
-    <?php get_template_part('template-parts/sections/sec-2');?>
-
-  </div>
-
-  <?php get_template_part('template-parts/sections/video-reviews');?>
-
-  <div class="space-sec"></div>
-
-  <?php get_template_part('template-parts/sections/quiz');?>
-
-  <div class="wrapper-brown">
-
-    <?php get_template_part('template-parts/sections/verified-materials-2');?>
-
-    <div class="divider"></div>
-
-    <?php get_template_part('template-parts/sections/pasting-options'); ?>
-
-    <div class="space-sec"></div>
-
-  </div>
-
-  <?php get_template_part('template-parts/sections/sec-7'); ?>
-
-  <?php get_template_part('template-parts/sections/perfect-result'); ?>
-
-  <div class="wrapper-brown">
-
-    <?php get_template_part('template-parts/sections/quality-pasting'); ?>
-
-    <div class="space-sec"></div>
-
-  </div>
-
-  <?php get_template_part('template-parts/sections/sec-10'); ?>
-
-  <?php get_template_part('template-parts/sections/sec-11'); ?>
-
-  <?php get_template_part('template-parts/sections/sec-12'); ?>
-
-  <?php get_template_part('template-parts/sections/complex-service');?>
 
 
-  <div class="wrapper-brown">
+  <?php renderBlocks();?>
 
-    <?php get_template_part('template-parts/sections/sec-14'); ?>
-
-  </div>
+  <?php get_template_part('template-parts/sections/sec-14'); ?>
 
   <?php get_template_part('template-parts/sections/map'); ?>
-
 
 </main>
 
 <?php get_footer(); ?>
+
+
+<!-- type-first-block : type-first-block
+carcase-info : carcase-info
+skin-happen : skin-happen
+skin-option : skin-option
+full-wrapping : full-wrapping
+verified-materials-2 : verified-materials-2
+pasting-options : pasting-options
+skin-listimg : skin-listimg
+polishing-types : polishing-types
+materials-manufacturers : materials-manufacturers
+pasting-of-any-complexity : pasting-of-any-complexity
+sec-2 : sec-2
+video-reviews : video-reviews
+quiz : quiz
+sec-7 : sec-7
+perfect-result : perfect-result
+quality-pasting : quality-pasting
+sec-10 : sec-10
+sec-11 : sec-11
+sec-12 : sec-12
+complex-service : complex-service
+wrapper-start : Обертка
+wrapper-end : Конец обертки
+space : Отступ -->
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -39,3 +39,90 @@ add_action( 'wp_head', 'customthemes_pingback_header' );
 
 // Меню
 require get_template_directory() . '/inc/walkerMenu.php';
+
+
+
+// Функция вывода блоков
+
+function renderBlocks(){
+	if(have_rows('sortBlocks')) {
+		while(have_rows('sortBlocks')) {
+			the_row();
+			$type_block = get_sub_field('type_block');
+			switch ($type_block){
+				case 'type-first-block':
+					get_template_part('template-parts/sections/type-first-block');
+					break;
+				case 'carcase-info':
+					get_template_part('template-parts/sections/carcase-info');
+					break;
+				case 'skin-happen':
+					get_template_part('template-parts/sections/skin-happen');
+					break;
+				case 'skin-option':
+					get_template_part('template-parts/sections/skin-option');
+					break;
+				case 'full-wrapping':
+					get_template_part('template-parts/sections/full-wrapping');
+					break;
+				case 'verified-materials-2':
+					get_template_part('template-parts/sections/verified-materials-2');
+					break;
+				case 'pasting-options':
+					get_template_part('template-parts/sections/pasting-options');
+					break;
+				case 'skin-listimg':
+					get_template_part('template-parts/sections/skin-listimg');
+					break;
+				case 'polishing-types':
+					get_template_part('template-parts/sections/polishing-types');
+					break;
+				case 'materials-manufacturers':
+					get_template_part('template-parts/sections/materials-manufacturers');
+					break;
+				case 'pasting-of-any-complexity':
+					get_template_part('template-parts/sections/pasting-of-any-complexity');
+					break;
+				case 'sec-2':
+					get_template_part('template-parts/sections/sec-2');
+					break;
+				case 'video-reviews':
+					get_template_part('template-parts/sections/video-reviews');
+					break;
+				case 'quiz':
+					get_template_part('template-parts/sections/quiz');
+					break;
+				case 'sec-7':
+					get_template_part('template-parts/sections/sec-7');
+					break;
+				case 'perfect-result':
+					get_template_part('template-parts/sections/perfect-result');
+					break;
+				case 'quality-pasting':
+					get_template_part('template-parts/sections/quality-pasting');
+					break;
+				case 'sec-10':
+					get_template_part('template-parts/sections/sec-10');
+					break;
+				case 'sec-11':
+					get_template_part('template-parts/sections/sec-11');
+					break;
+				case 'sec-12':
+					get_template_part('template-parts/sections/sec-12');
+					break;
+				case 'complex-service':
+					get_template_part('template-parts/sections/complex-service');
+					break;
+				case 'wrapper-start':
+					echo '<div class="wrapper-brown">';
+					break;
+				case 'wrapper-end':
+					echo '</div>';
+					break;
+				case 'space':
+					echo '<div class="space-sec"></div>';
+					break;
+			}
+		}
+	}
+}
